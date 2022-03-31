c947904eca97d2aa79856a1152f49472c7208641
asierMA <amayoz003@ikasle.ehu.eus>
2022-03-31 19:50:21 +0200

    setFees

--- src/main/java/uicontrollers/setFeesController.java
+++ src/main/java/uicontrollers/setFeesController.java
@@ -1,7 +1,9 @@
 package uicontrollers;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.*;
 
 import businessLogic.BlFacade;
 import domain.Event;
@@ -11,14 +13,13 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.skin.DatePickerSkin;
+import javafx.util.Callback;
 import ui.MainGUI;
+import utils.Dates;
 
 public class setFeesController implements Controller {
 
@@ -72,6 +73,8 @@ public class setFeesController implements Controller {
 
     private ObservableList eventData;
 
+    private List<LocalDate> holidays = new ArrayList<>();
+
     public setFeesController(BlFacade bl) {
         businessLogic = bl;
     }
@@ -83,38 +86,134 @@ public class setFeesController implements Controller {
 
     @FXML
     void setFeeClick(ActionEvent event) {
-        if(dateSelector.getValue() == null){
-            answerLbl.setText("You must select a date");
+
+        answerLbl.getStyleClass().clear();
+        try{
+            if(dateSelector.getValue() == null){
+                answerLbl.setText("You must select a date");
+                answerLbl.getStyleClass().setAll("lbl","lbl-danger");
+            }
+            else if(eventTable.getSelectionModel().getSelectedItem() == null){
+                answerLbl.setText("You must select an event");
+                answerLbl.getStyleClass().setAll("lbl","lbl-danger");
+            }
+            else if(questionTable.getSelectionModel().getSelectedItem() == null){
+                answerLbl.setText("You must select a question");
+                answerLbl.getStyleClass().setAll("lbl","lbl-danger");
+            }
+            else if(resultField.getText().equals("")){
+                answerLbl.setText("You must enter a result");
+                answerLbl.getStyleClass().setAll("lbl","lbl-danger");
+            }
+            else if(feeField.getText().equals("")){
+                answerLbl.setText("You must select a fee");
+                answerLbl.getStyleClass().setAll("lbl","lbl-danger");
+            }
+            //else if(businessLogic.feeExists(answerLbl.getText(),(String) questionTableModel.getValueAt(j,1))) {
+            //    answerLbl.setText("You must select another result.");
+            //}
+            else{
+                Event selectedEvent = eventTable.getSelectionModel().getSelectedItem();
+                String question = questionTable.getSelectionModel().getSelectedItem().getQuestion();
+                businessLogic.setFee(resultField.getText(), Float.parseFloat(feeField.getText())
+                        , question, selectedEvent);
+                answerLbl.getStyleClass().clear();
+                answerLbl.getStyleClass().setAll("lbl", "lbl-success");
+                answerLbl.setText("Fee correctly added");
+            }
+        }
+        catch (NumberFormatException ex){
+            answerLbl.setText("You must enter a number");
             answerLbl.getStyleClass().setAll("lbl","lbl-danger");
         }
-        else if(eventTable.getSelectionModel().getSelectedItem() == null){
-            answerLbl.setText("You must select an event");
-            answerLbl.getStyleClass().setAll("lbl","lbl-danger");
+
+    }
+
+    private void setEvents(int year, int month) {
+
+        Date date = Dates.toDate(year, month);
+
+        for (Date day : businessLogic.getEventsMonth(date)) {
+            holidays.add(Dates.convertToLocalDateViaInstant(day));
         }
-        else if(questionTable.getSelectionModel().getSelectedItem() == null){
-            answerLbl.setText("You must select a question");
-            answerLbl.getStyleClass().setAll("lbl","lbl-danger");
-        }
-        else if(resultField.getText().equals("")){
-            answerLbl.setText("You must enter a result");
-            answerLbl.getStyleClass().setAll("lbl","lbl-danger");
-        }
-        else if(feeField.getText().equals("")){
-            answerLbl.setText("You must select a fee");
-            answerLbl.getStyleClass().setAll("lbl","lbl-danger");
-        }
-        //else if(businessLogic.feeExists(answerLbl.getText(),(String) questionTableModel.getValueAt(j,1))) {
-        //    answerLbl.setText("You must select another result.");
-        //}
+    }
+
+    private void setEventsPrePost(int year, int month) {
+        LocalDate date = LocalDate.of(year, month, 1);
+        setEvents(date.getYear(), date.getMonth().getValue());
+        setEvents(date.plusMonths(1).getYear(), date.plusMonths(1).getMonth().getValue());
+        setEvents(date.plusMonths(-1).getYear(), date.plusMonths(-1).getMonth().getValue());
     }
 
     @FXML
     void initialize() {
-        questionId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        questionTxt.setCellValueFactory(new PropertyValueFactory<>("text"));
 
-        eventId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        eventTxt.setCellValueFactory(new PropertyValueFactory<>("text"));
+        setEventsPrePost(LocalDate.now().getYear(), LocalDate.now().getMonth().getValue());
+
+
+        // get a reference to datepicker inner content
+        // attach a listener to the  << and >> buttons
+        // mark events for the (prev, current, next) month and year shown
+        dateSelector.setOnMouseClicked(e -> {
+            DatePickerSkin skin = (DatePickerSkin) dateSelector.getSkin();
+            skin.getPopupContent().lookupAll(".button").forEach(node -> {
+                node.setOnMouseClicked(event -> {
+                    List<Node> labels = skin.getPopupContent().lookupAll(".label").stream().toList();
+                    String month = ((Label) (labels.get(0))).getText();
+                    String year = ((Label) (labels.get(1))).getText();
+                    YearMonth ym = Dates.getYearMonth(month + " " + year);
+                    setEventsPrePost(ym.getYear(), ym.getMonthValue());
+                });
+            });
+
+
+        });
+
+        dateSelector.setDayCellFactory(new Callback<DatePicker, DateCell>() {
+            @Override
+            public DateCell call(DatePicker param) {
+                return new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        if (!empty && item != null) {
+                            if (holidays.contains(item)) {
+                                this.setStyle("-fx-background-color: pink");
+                            }
+                        }
+                    }
+                };
+            }
+        });
+
+        dateSelector.setOnAction(actionEvent -> {
+            eventTable.getItems().clear();
+            questionTable.getItems().clear();
+            resultField.setText("");
+            feeField.setText("");
+            answerLbl.setText("");
+            answerLbl.getStyleClass().clear();
+            Vector<Event> events= businessLogic.getEvents(Dates.convertToDate(dateSelector.getValue()));
+            for(Event e:events){
+                eventTable.getItems().add(e);
+            }
+        });
+
+        eventTable.getSelectionModel().selectedItemProperty().addListener((obs, oldselection, newselection) -> {
+            if(newselection != null){
+                questionTable.getItems().clear();
+                Vector<Question> questions = eventTable.getSelectionModel().getSelectedItem().getQuestions();
+                for(Question q:questions){
+                    questionTable.getItems().add(q);
+                }
+            }
+        });
+        questionId.setCellValueFactory(new PropertyValueFactory<>("questionNumber"));
+        questionTxt.setCellValueFactory(new PropertyValueFactory<>("question"));
+
+        eventId.setCellValueFactory(new PropertyValueFactory<>("eventNumber"));
+        eventTxt.setCellValueFactory(new PropertyValueFactory<>("description"));
 
     }
 
