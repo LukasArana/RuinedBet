507c4f398c618a4bdbf2ddcf1e8b264a5dd8e6eb
LukasArana <lukas.ara.gar@gmail.com>
2022-05-10 11:14:38 +0200

    Fixing bugs

--- src/main/java/businessLogic/BlFacade.java
+++ src/main/java/businessLogic/BlFacade.java
@@ -57,7 +57,7 @@ public interface BlFacade  {
 	
 	@WebMethod public boolean emailIsFree(String email);
 	@WebMethod public boolean checkLogIn(String username, String password);
-	@WebMethod public boolean isAdmin(String username);
+	@WebMethod public boolean isAdmin();
 	
 	@WebMethod public Event createEvent(String description,Date date) throws EventAlreadyExists;
 	
@@ -73,8 +73,6 @@ public interface BlFacade  {
 
 	void setUser(String username);
 	User getCurrentUser();
-
-	@WebMethod User getCurrentUser(String username);
 
     @WebMethod void payWinners(Question q, fee f);
 }
